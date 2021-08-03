b07810c1a95914d8847ad7007ebf6d66afa54959
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2021-08-03 12:10:52 +0530

    Update Fixed_XOR.py

--- Set 1/Python/Fixed_XOR.py
+++ Set 1/Python/Fixed_XOR.py
@@ -1,24 +1,16 @@
-'''
-  
-    Write a function that takes two equal-length buffers and produces their XOR combination.
-
-    If your function works properly, then when you feed it the string:
- 
-    1c0111001f010100061a024b53535009181c
-
-   ... after hex decoding, and when XOR'd against:
-
-    686974207468652062756c6c277320657965
-    
-   ... should produce:
-
-   746865206b696420646f6e277420706c6179
-   
-'''   
 from pwn import xor
 from binascii import hexlify, unhexlify
+
+#Input Strings
 string1 = '1c0111001f010100061a024b53535009181c'  
 string2 = '686974207468652062756c6c277320657965'
-string1 = unhxelify(string1)
-string = unhexlify(string2)
-output = hexlify(xor(string1,string2))
+
+def xor():   
+    string1 = unhxelify(string1)
+    string = unhexlify(string2)
+    output = hexlify(xor(string1,string2))
+    print(output)
+    
+xor()    
+
+#Output = 746865206b696420646f6e277420706c6179
