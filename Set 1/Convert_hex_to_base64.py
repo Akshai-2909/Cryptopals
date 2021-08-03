3f79a9f0a80b61dea47e0c8ecda37e215fbbdfd1
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2021-08-03 09:30:12 +0530

    Update Convert_hex_to_base64.py

--- Set 1/Convert_hex_to_base64.py
+++ Set 1/Convert_hex_to_base64.py
@@ -4,7 +4,9 @@
   Output = SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 
 '''
+from binascii import unhexlify
 import base64
 string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+string = unhexlify(string)
 output = base64.b64encode(string)
 print(output)
