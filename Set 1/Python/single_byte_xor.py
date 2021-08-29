7e7500ec7dbc721331053142267a1b05af4940ca
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2021-08-29 16:35:30 +0530

    Update single_byte_xor.py

--- Set 1/Python/single_byte_xor.py
+++ Set 1/Python/single_byte_xor.py
@@ -2,8 +2,7 @@ from binascii import hexlify , unhexlify
 from pwn import xor
 
 charSet= 'ABCDEFGHIJKLMNOPQRSTUVXYWZabcdefghijklmnopqrstuvwxyz'
-string= '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736' #Input String
-string = unhexlify(string)
+string = unhexlify('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
 length = len(charSet)
 def xor():
 for i in range(length):
